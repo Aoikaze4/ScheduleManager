ce282a4072f51fe3d72753c544a9edf229a0a4ab
Aoikaze <kaze4border@gmail.com>
2021-08-28 21:16:07 +0000

    [update]gem version

--- ScheduleManager/config/routes.rb
+++ ScheduleManager/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, only: [:sign_up,:sign_in,:sign_out,:session]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'homes#top'
   get 'top', to: 'homes#top'
   get 'about', to: 'homes#about'
-  
+
+  resources :users, only:[:show,:edit,:update]
+
 end
