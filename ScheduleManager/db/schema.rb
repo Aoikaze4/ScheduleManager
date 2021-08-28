1f6ffdaf8f7c4656ecaea8646100786303fa3cc4
Aoikaze <kaze4border@gmail.com>
2021-08-28 22:20:37 +0000

    [update]migration, added event_image table, added image_id column to User Table

--- ScheduleManager/db/schema.rb
+++ ScheduleManager/db/schema.rb
@@ -10,48 +10,55 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210828195059) do
+ActiveRecord::Schema.define(version: 2021_08_28_221456) do
 
   create_table "comments", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "event_id"
-    t.integer  "image_id"
-    t.string   "comment"
+    t.integer "user_id"
+    t.integer "event_id"
+    t.integer "image_id"
+    t.string "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "event_images", force: :cascade do |t|
+    t.integer "event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "events", force: :cascade do |t|
-    t.integer  "user_id"
+    t.integer "user_id"
     t.datetime "event_at"
-    t.string   "event_name"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string "event_name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "favorites", force: :cascade do |t|
-    t.integer  "event_id"
-    t.integer  "user_id"
+    t.integer "event_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "images", force: :cascade do |t|
-    t.integer  "user_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",                                null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string "name", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
