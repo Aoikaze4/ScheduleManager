9c57f333e37d9695c5eed89c939a455b73e40fa6
Aoikaze <kaze4border@gmail.com>
2021-08-28 23:24:58 +0000

    [fix]fixed datatype of image_id, integer => string

--- ScheduleManager/db/schema.rb
+++ ScheduleManager/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_28_223109) do
+ActiveRecord::Schema.define(version: 2021_08_28_232304) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 2021_08_28_223109) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "image_id"
+    t.string "image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
