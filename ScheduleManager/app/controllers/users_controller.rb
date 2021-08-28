40bc5ad4fa5882c4e98282d142a481e587a28e9e
Aoikaze <kaze4border@gmail.com>
2021-08-28 22:00:42 +0000

    [update]devise controller users, after sign_in_path

--- ScheduleManager/app/controllers/users_controller.rb
+++ ScheduleManager/app/controllers/users_controller.rb
@@ -1,5 +1,23 @@
 class UsersController < ApplicationController
+  def mypage
+    @user = User.find(current_user.id)
+  end
+
   def edit
     @user = User.find(current_user.id)
+  end
+
+  def update
+    user = User.find(current_user.id)
+    if user.update(user_params)
+      redirect_to my_page_path, notice: "編集が完了しました。"
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name,:email)
   end
 end
--- ScheduleManager/config/routes.rb
+++ ScheduleManager/config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users, only: [:sign_up,:sign_in,:sign_out,:session]
+  devise_for :users, controllers: {sessions: 'users/sessions', registrations: 'users/registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'homes#top'
   get 'top', to: 'homes#top'
   get 'about', to: 'homes#about'
 
-  resources :users, only:[:show,:edit,:update]
+  resources :users, only:[:edit,:update]
+  get 'mypage', to: 'users#mypage'
 
 end
