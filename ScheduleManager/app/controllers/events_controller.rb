b35fb72f84d9e4bb7a46dbff9cb116a14bfb5379
Aoikaze <kaze4border@gmail.com>
2021-09-02 00:59:20 +0000

    [update]event.new html, function, added validates to event model

--- ScheduleManager/app/controllers/events_controller.rb
+++ ScheduleManager/app/controllers/events_controller.rb
@@ -1,2 +1,17 @@
 class EventsController < ApplicationController
+  def new
+    @event = Event.new
+  end
+
+  def create
+    event = Event.new(event_params)
+    if event.save!
+      redirect_to event_path(event.id)
+    end
+  end
+
+  private
+  def event_params
+
+  end
 end
--- ScheduleManager/app/models/event.rb
+++ ScheduleManager/app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
   has_many :user
   has_many :comment
+
+  validates :event_name,:event_at, presence: true
+  validates :event_name, length: {maximum: 20}
 end
