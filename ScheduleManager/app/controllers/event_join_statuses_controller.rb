55ba90ba91d4d38f9aa5a730fcd74f2b3344afcd
Aoikaze <kaze4border@gmail.com>
2021-09-13 05:07:02 +0000

    [fix]fixed conditions to display about edit_event_path's link_to

--- ScheduleManager/app/controllers/event_join_statuses_controller.rb
+++ ScheduleManager/app/controllers/event_join_statuses_controller.rb
@@ -15,11 +15,18 @@ class EventJoinStatusesController < ApplicationController
 
   def update
     event_join_status = EventJoinStatus.find(params[:id])
-    event = event_join_status.event_id
     if event_join_status.update(event_join_status_params)
-      redirect_to event_path(event)
+      redirect_to event_path(event_join_status.event_id)
     end
   end
+
+  def destroy
+    event_join_status = EventJoinStatus.find(params[:id])
+    if event_join_status.destroy
+      redirect_to event_path(event_join_status.event_id)
+    end
+  end
+
 
   private
   def event_join_status_params
